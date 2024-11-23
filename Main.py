5bdb44bc67d57b21d12428727d1b9adb71eabb07
Genesplicer67 <colinjschotte@gmail.com>
2024-11-23 20:48:08 +0800

    Added basic bot function (so it loads and does not kill).

--- Main.py
+++ Main.py
@@ -1,1 +1,20 @@
-print("Heloo")
+import discord
+from discord.ext import commands
+
+# Replace this with your bot's token
+TOKEN = 'TOKEN'
+
+intents = discord.Intents.default()
+intents.members = True  # To detect members joining/leaving the server
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+# Event: Bot is ready
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}!')
+    print('Bot is ready!')
+
+# Run the bot
+bot.run(TOKEN)
